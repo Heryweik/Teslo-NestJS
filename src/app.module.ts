0f75fe0a9174ded9928bfde45cb02d2f41b6a19c
Yhonny Aplicano <yhonny296@gmail.com>
2024-08-18 14:46:44 -0600

    [Add] - Conexion a SupaBase + configuracion de conexion certificada (No se si funciona)

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,25 +17,44 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
     ConfigModule.forRoot(), // Configuracion de variables de entorno
 
     TypeOrmModule.forRoot({
+      // Conexion certificada
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl:
+          process.env.STAGE === 'prod'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
+
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      // Funcionamiento local
+      // host: process.env.DB_HOST,
+      // port: +process.env.DB_PORT,
+      // database: process.env.DB_NAME,
+      // username: process.env.DB_USER,
+      // password: process.env.DB_PASSWORD,
+
+      // BD en la nube, tambien se pueden usar los parametros de arriba, supabse los proporciona en configuracion
+      url: process.env.DATABASE_URL,
+
       autoLoadEntities: true, // Carga las entidades automaticamente
       synchronize: true, // Solo para desarrollo (crea las tablas automaticamente)
-    }), ProductsModule, CommonModule, SeddModule, FilesModule,
+    }),
+    ProductsModule,
+    CommonModule,
+    SeddModule,
+    FilesModule,
 
     // Configuración de la carpeta estática
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname,'..','public'),
-      }),
+      rootPath: join(__dirname, '..', 'public'),
+    }),
 
     AuthModule,
 
-    MessagesWsModule 
+    MessagesWsModule,
   ],
 })
-
 export class AppModule {}
